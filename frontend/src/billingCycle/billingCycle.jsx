5d520a058f45e1e3c17dd8676099af0b5ebdc817
Walter Santos Filho <wcsantosfilho@gmail.com>
2018-06-05 18:35:34 -0300

    List e Formularios

---
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabContent from '../common/tab/tabContent'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActions'
+import List from './billingCycleList'
+import Form from './billingCycleForm'
+
+class BillingCycle extends Component {
+    componentWillMount() {
+        this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader lable='Listar' icon='bars' target='tabList' />
+                            <TabHeader lable='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader lable='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader lable='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create} />
+                            </TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
+
+// wrapper é uma classe do AdminLTE
+export default props => (
+    <div className='wrapper'>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
+    </div>
+)
